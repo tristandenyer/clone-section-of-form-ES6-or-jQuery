d0becafe65fb0256cb0d193501e633f2c7ce92d8
tristandenyer <tristan@tristandenyer.com>
2013-08-25 13:58:01 -0700

    Better comments
    Some of the inline comments were a bit vague, so I expanded on them a
    bit.

--- clone-form-td.js
+++ clone-form-td.js
@@ -1,15 +1,21 @@
 /*
 Author: Tristan Denyer (based on Charlie Griefer's original clone code, and some great help from Dan - see his comments in blog post)
 Plugin and demo at http://tristandenyer.com/using-jquery-to-duplicate-a-section-of-a-form-maintaining-accessibility/
-Ver: 0.9.3
-Date: Mar 3, 2013
+Ver: 0.9.4
+Date: Aug 25, 2013
 */
 $(function () {
     $('#btnAdd').click(function () {
-        var num     = $('.clonedInput').length, // how many "duplicatable" input fields we currently have
-            newNum  = new Number(num + 1),      // the numeric ID of the new input field being added
+        var num     = $('.clonedInput').length, // Checks to see how many "duplicatable" input fields we currently have
+            newNum  = new Number(num + 1),      // The numeric ID of the new input field being added, increasing by 1 each time
             newElem = $('#entry' + num).clone().attr('id', 'entry' + newNum).fadeIn('slow'); // create the new element via clone(), and manipulate it's ID using newNum value
-    // manipulate the name/id values of the input inside the new element
+    
+    /*  This is where we manipulate the name/id values of the input inside the new, cloned element
+        Below are examples of what forms elements you can clone, but not the only ones.
+        There are 2 basic structures below: one for an H2, and one for form elements.
+        To make more, you can copy the one for form elements and simply update the classes for its label and input.
+        Keep in mind that the .val() method is what clears the element when it gets cloned. Radio and checkboxes need .val([]) instead of .val('').
+    */
         // H2 - section
         newElem.find('.heading-reference').attr('id', 'ID' + newNum + '_reference').attr('name', 'ID' + newNum + '_reference').html('Entry #' + newNum);
 
@@ -37,35 +43,34 @@ $(function () {
         newElem.find('.label_email').attr('for', 'ID' + newNum + '_email_address');
         newElem.find('.input_email').attr('id', 'ID' + newNum + '_email_address').attr('name', 'ID' + newNum + '_email_address').val('');
 
-    // insert the new element after the last "duplicatable" input field
+    // Insert the new element after the last "duplicatable" input field
         $('#entry' + num).after(newElem);
         $('#ID' + newNum + '_title').focus();
 
-    // enable the "remove" button
+    // Enable the "remove" button. This only shows once you have a duplicated section.
         $('#btnDel').attr('disabled', false);
 
-    // right now you can only add 5 sections. change '5' below to the max number of times the form can be duplicated
+    // Right now you can only add 4 sections, for a total of 5. Change '5' below to the max number of sections you want to allow.
         if (newNum == 5)
         $('#btnAdd').attr('disabled', true).prop('value', "You've reached the limit");
     });
 
     $('#btnDel').click(function () {
-    // confirmation
+    // Confirmation dialog box
         if (confirm("Are you sure you wish to remove this section? This cannot be undone."))
             {
                 var num = $('.clonedInput').length;
                 // how many "duplicatable" input fields we currently have
-                $('#entry' + num).slideUp('slow', function () {$(this).remove(); 
+                $('#entry' + num).slideUp('slow', function () {$(this).remove();
                 // if only one element remains, disable the "remove" button
                     if (num -1 === 1)
                 $('#btnDel').attr('disabled', true);
                 // enable the "add" button
                 $('#btnAdd').attr('disabled', false).prop('value', "add section");});
             }
-        return false;
-             // remove the last element
+        return false; // Removes the last section you added
 
-    // enable the "add" button
+    // Enable the "add" button
         $('#btnAdd').attr('disabled', false);
     });
 
